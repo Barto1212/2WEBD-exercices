ad339c62ff41c14a3d82864c61d6b390c8299911
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-04-30 11:41:37 +0200

    before exo chap3p19

---
+++ jour2/chap2-p13/mon_super_composant.jsx
@@ -0,0 +1,6 @@
+const MonSuperComposant = () => {
+  return <h1>React is Here</h1>;
+};
+
+const container = document.getElementById("react-container");
+ReactDOM.createRoot(container).render(<MonSuperComposant />);
